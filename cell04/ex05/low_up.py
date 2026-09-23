c6c42a33ba5f067529810d3ff5d338ae57438933
Mangkorn12 <kongidagon.o@gmail.com>
2026-09-23 22:53:49 +0700

    addcell04

---
+++ cell04/ex05/low_up.py
@@ -0,0 +1,12 @@
+text = input()
+result = ""
+
+for char in text:
+    if 'a' <= char <= 'z':
+        result += chr(ord(char) - 32)
+    elif 'A' <= char <= 'Z':
+        result += chr(ord(char) + 32)
+    else:
+        result += char
+
+print(result)
