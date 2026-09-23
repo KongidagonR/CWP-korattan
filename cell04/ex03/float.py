ee54169451c5c9adabccf762392dea0cecda48f6
Mangkorn12 <kongidagon.o@gmail.com>
2026-09-23 22:39:21 +0700

    cell04

---
+++ cell04/ex03/float.py
@@ -0,0 +1,6 @@
+a = float(input("Give me a number: "))
+
+if a.is_integer():
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,4 @@
+import math
+
+a = float(input("Give me a number: "))
+print(math.ceil(a))
